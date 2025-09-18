41bd155c38a467f6057112333625f8505ab27395
Isaac <isaac.j.mayo@gmail.com>
2025-09-18 18:07:33 -0500

    Initial Commit of launchWheels.java and westCoastTeleOp.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/launchWheels.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Assemblies;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class launchWheels {
+
+    // init wheels
+    DcMotor left;
+    DcMotor right;
+
+    launchWheels() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/westCoastTeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "ArgoTeleOp", group  = "LinearOpMode")
+public class westCoastTeleOp extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+}
