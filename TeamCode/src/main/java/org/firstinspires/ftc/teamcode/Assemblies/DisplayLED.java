08b283e12a66b57f61ba1996d4fa3d70e278362b
kahvi <kahvidsachi@gmail.com>
2025-09-20 16:23:01 -0500

    Setting up variables & adding comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/DisplayLED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/DisplayLED.java
@@ -11,6 +11,10 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class DisplayLED {
+    public int WorksGreat = Color.CYAN; // Does this when if intake flywheel & wall servo work in sync
+    public int WorksBad = Color.RED;    // Does this when intake flywheel works but not the wall servo
+    public int WorksOkay = Color.BLUE;  // Does this when wall servo works but not the intake flywheel
+    public int WorksOff = Color.BLACK;  // Does this when the wall servo & intake flywheels aren't on
     private SparkFunLEDStick LEDLight;
     Robot robot = new Robot(hardwareMap, telemetry);
 
@@ -20,13 +24,13 @@ public class DisplayLED {
     public void IntakeCheck(boolean lightOn) {
         while (lightOn) { // uhhh why is `LEDLight` producing an error"
             if (!robot.escarGOMech.intakeFlywheelLeft.equals(0) && robot.escarGOMech.wallServo.equals(robot.escarGOMech.closePos)) {
-                LEDLight.setColor(Color.CYAN); // If intake flywheel & wall servo work in sync
+                LEDLight.setColor(WorksGreat); // If the intake flywheel & wall servo work in sync
             } else if (!robot.escarGOMech.intakeFlywheelLeft.equals(0) && robot.escarGOMech.wallServo.equals(robot.escarGOMech.openPos)) {
-                LEDLight.setColor(Color.GREEN); // if intake flywheel works but not the wall servo
+                LEDLight.setColor(WorksBad); // if the intake flywheel works but not the wall servo
             } else if (robot.escarGOMech.intakeFlywheelLeft.equals(0) && robot.escarGOMech.wallServo.equals(robot.escarGOMech.closePos)) {
-                LEDLight.setColor(Color.BLUE); // if the wall servo works but not the intake flywheel
+                LEDLight.setColor(WorksOkay); // if the wall servo works but not the intake flywheel
             } else {
-                LEDLight.setColor(Color.BLACK); // if the wall servo & intake flywheels aren't on
+                LEDLight.setColor(WorksOff); // if the wall servo & intake flywheels aren't on
             }
         }
     }
