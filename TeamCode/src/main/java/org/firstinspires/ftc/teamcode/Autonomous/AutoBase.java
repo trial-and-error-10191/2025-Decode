3f3c2023ec168a17e966e4fc2dfbae42bcc3f16f
kahvi <kahvidsachi@gmail.com>
2025-09-24 14:54:15 -0500

    Adding stuff to the auto base file, no paths yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-public class AutoBase {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Assemblies.DriveTrain;
+import org.firstinspires.ftc.teamcode.Assemblies.EscarGOMech;
+
+@Autonomous
+public class AutoBase extends LinearOpMode {
+    AprilTagDetect aprilTag;
+    DriveTrain drive;
+    EscarGOMech launcher;
+
+    private final ElapsedTime Time = new ElapsedTime();
+
+    public void runOpMode() {
+        aprilTag = new AprilTagDetect();
+        launcher = new EscarGOMech(hardwareMap, telemetry);
+        drive    = new DriveTrain(hardwareMap, telemetry);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Path stuff maybe?
+        }
+    }
+    public void Wait(double seconds) {
+        Time.reset();
+        while (Time.milliseconds()  < seconds * 1000) {
+            // doesn't need anything
+        } // end of while loop
+    } // end of public void Wait
 }
