b390f407b87bd5efeb89034cf4043c21d4f655aa
isaac <isaac.j.mayo@gmail.com>
2025-09-23 17:56:16 -0700

    Basic code for paddle loader

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/ArtifactPaddles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/ArtifactPaddles.java
@@ -2,15 +2,35 @@ package org.firstinspires.ftc.teamcode.Assemblies;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class ArtifactPaddles {
+
+    /* create the servo */
     CRServo paddles;
+
+    /* parameter variables */
+    int oneMS = 0;
+    int onePower = 0;
+
+    /* location variables */
+    int State = 1;
+
+    /* time */
+    ElapsedTime runTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
 
     ArtifactPaddles(Telemetry telemetry, HardwareMap hwMap) {
         paddles = hwMap.get(CRServo.class, "paddles");
     }
 
 
+    public void setRot(int state) {
+        double time = runTime.milliseconds();
+        boolean pos = (this.State - state) > 0;
+        while (Math.abs(this.State - state) * oneMS > runTime.milliseconds() - time) {
+            paddles.setPower(onePower);
+        }
+    }
 }
