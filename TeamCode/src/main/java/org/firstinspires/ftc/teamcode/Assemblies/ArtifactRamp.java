d2472b9be3fa5db9a4e0aff18b8c3fb076d8515d
emm02 <emm021810@gmail.com>
2025-09-25 18:20:53 -0500

    Classes edited

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/ArtifactRamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/ArtifactRamp.java
@@ -1,10 +1,46 @@
 package org.firstinspires.ftc.teamcode.Assemblies;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ArtifactRamp {
 
+    static double INCREMENT = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final double MAX_POS = 1.0;     // Maximum rotational position
+    static final double MIN_POS = 0.0;     // Minimum rotational position
+    double rotatePosition = 0.5;                  // Start at halfway position
+
     // Define class members
-    Servo   servo;
+    public static Servo RampServo;
+
+
+
+
+    public void rampServo (boolean up, boolean down) {
+
+    if (up) {                                     // lifts ramp up
+        rotatePosition += INCREMENT;
+        if (rotatePosition >= MAX_POS) {
+            rotatePosition = MAX_POS;
+        }
+    } else if (down) {                               // lowers ramp down
+        rotatePosition -= INCREMENT;
+        if (rotatePosition <= MIN_POS) {
+            rotatePosition = MIN_POS;
+        }
+    }
+    RampServo.setPosition(rotatePosition);
+}
+
+
+
+
+
+
+
+
+
+
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ArtifactRampTeleOp.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Assemblies.ArtifactRamp;
+import org.firstinspires.ftc.teamcode.Assemblies.Robot;
+
+@TeleOp(name = "ArtifactRampOperation", group  = "LinearOpMode")
+public class ArtifactRampTeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Initiates the robots system and subsystems!
+        Robot robot = new Robot(hardwareMap, telemetry);
+
+        waitForStart();
+
+        ArtifactRamp servo = new ArtifactRamp();
+
+        }
+}
