08c5af47899e9d0a1cb6a1603a5ec1b4baa68be6
emm02 <emm021810@gmail.com>
2025-10-16 19:27:02 -0500

    Class edited

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ArtifactRampTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ArtifactRampTeleOp.java
@@ -12,12 +12,19 @@ public class ArtifactRampTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        // Initiates the robots system and subsystems!
+        // Initiates the robots system and subsystems.
         Robot robot = new Robot(hardwareMap, telemetry);
 
         waitForStart();
 
-        ArtifactRamp servo = new ArtifactRamp();
+        ArtifactRamp servo = new ArtifactRamp(telemetry, hardwareMap);
+
+        while (opModeIsActive()) {
+
+            servo.rampServo(gamepad2.dpad_up, gamepad2.dpad_down);
+
+            telemetry.update();
+        }
 
         }
 }
