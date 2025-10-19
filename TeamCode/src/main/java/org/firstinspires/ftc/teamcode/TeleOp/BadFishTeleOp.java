eb9a07d1385e331a02b60937d41476950411634d
kahvi <kahvidsachi@gmail.com>
2025-10-18 22:06:33 -0500

    Moving some code to here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BadFishTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BadFishTeleOp.java
@@ -3,14 +3,22 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Assemblies.Robot;
+
 @TeleOp
 public class BadFishTeleOp extends LinearOpMode {
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
+        // Initiates the robots system and subsystems!
+        Robot robot = new Robot(hardwareMap, telemetry);
+
+        telemetry.addData("Status", "Waiting for Start");
+        telemetry.update();
+
         waitForStart();
-
-        while (opModeIsActive()) {
-
+        while(opModeIsActive()) {
+            robot.driveTrain.drive(-gamepad1.left_stick_y, gamepad1.right_stick_x);
+            robot.displayLED.IntakeCheck(true);
         }
     }
 }
