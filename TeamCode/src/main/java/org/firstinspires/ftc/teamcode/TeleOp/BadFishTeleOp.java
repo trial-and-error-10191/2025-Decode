6a3b035e456dc1e4f155a57bc4692951ba6a32b4
kahvi <kahvidsachi@gmail.com>
2025-10-18 21:58:59 -0500

    Moving some code to here

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BadFishTeleOp.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class BadFishTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+}
