04722ed4950dea4db22c4131d743c89d90be15b9
kahvi <kahvidsachi@gmail.com>
2025-10-18 22:10:57 -0500

    Moving some code to here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-public class AutoBase {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Assemblies.Robot;
+
+@Autonomous (name = "AutoBase", group = "Autonomous")
+public class AutoBase extends LinearOpMode {
+    private final ElapsedTime Time = new ElapsedTime();
+
+    public void runOpMode() {
+        Robot robot = new Robot(hardwareMap, telemetry);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            robot.aprilTagDetect.locateTargetAprilTag();
+            robot.displayLED.IntakeCheck(true);
+            // Path stuff maybe?
+        }
+    }
 }
