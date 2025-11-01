ef8e105cf848ef04617cd8d696e33914debc288a
isaac <isaac.j.mayo@gmail.com>
2025-11-01 13:11:11 -0700

    changes to ball release the my telemetry UI stuff before i make a new branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/ballRelease.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.Assemblies;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ballRelease {
+
+    // define the servo
+    Servo servo;
+
+    // define supportive vars
+    boolean previousInput = false;
+    boolean open = false;
+
+    public ballRelease(Telemetry telemetry, HardwareMap hwMap) {
+        servo = hwMap.get(Servo.class, "dropServo");
+    }
+
+    // function to drop the ball
+    public void Open() {
+        servo.setPosition(0);
+        open = true;
+    }
+
+    // function to close the hole for next ball
+    public void Close() {
+        servo.setPosition(0.5);
+        open = false;
+    }
+
+    public void releaseLogic(boolean release) {
+        if (!previousInput && release) {
+            if (open) {
+                Close();
+            } else {
+                Open();
+            }
+            previousInput = true;
+        }
+
+        if (!release) {
+            previousInput = false;
+        }
+    }
+}
