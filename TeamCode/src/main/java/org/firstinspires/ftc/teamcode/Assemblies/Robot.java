a2f3d0877d359a5877dbe78efc9508a9ee255480
kahvi <kahvidsachi@gmail.com>
2025-12-28 14:43:16 -0600

    Telling the robot where the camera is.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/Robot.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -50,10 +54,15 @@ public class Robot {
         order.add(Color.Purple);
         this.telemetry = telemetry;
 
+        Position cameraPosition = new Position(DistanceUnit.INCH,
+                0, 0, 0, 0);
+        YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+                0, -90, 90, 0);
+
         /// This first part sets up the camera so it can scan AprilTags
         // Create the AprilTag processor.
         aprilTag = new AprilTagProcessor.Builder()
-
+                .setCameraPose(cameraPosition, cameraOrientation)
                 .build();
 
         // Lets the camera see the obelisk April Tag from far away, as we only need to see that one once.
@@ -65,6 +74,8 @@ public class Robot {
 
 //        // Set and enable the processor.
         builder.addProcessor(aprilTag);
+
+
 //
 //        // Build the Vision Portal, using the above settings.
         visionPortal = builder.build();
