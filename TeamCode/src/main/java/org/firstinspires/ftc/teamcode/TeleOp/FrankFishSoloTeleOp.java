667cfa62ee5e314457fb8a208f45979d6d0faf38
isaac <isaac.j.mayo@gmail.com>
2026-01-20 17:59:47 -0800

    logic for toggle swapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FrankFishSoloTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FrankFishSoloTeleOp.java
@@ -9,6 +9,16 @@ import org.firstinspires.ftc.teamcode.Assemblies.Robot;
 
 @TeleOp (name = "FrankenFishSoloTeleOp", group = "LinearOpMode")
 public class FrankFishSoloTeleOp extends LinearOpMode {
+
+    enum mode {
+        ManualClose(),
+        ManualFar(),
+        Auto();
+    }
+    mode currentMode = mode.Auto;
+    boolean isLongPrevPressed = false;
+    boolean isShortPrevPressed = false;
+
     @Override
     public void runOpMode() {
         Robot robot = new Robot(hardwareMap, telemetry);
@@ -18,17 +28,46 @@ public class FrankFishSoloTeleOp extends LinearOpMode {
             robot.wheels.wheelsTick();
             robot.driveTrain.easingDrive(-gamepad1.left_stick_y, gamepad1.right_stick_x);
 
+
+            if (!isShortPrevPressed && !isLongPrevPressed) {
+                if (gamepad1.left_bumper) {
+                    if (currentMode.equals(mode.ManualFar)) {
+                        currentMode = mode.Auto;
+                    } else {
+                        currentMode = mode.ManualFar;
+                    }
+                }
+                if (gamepad1.left_trigger > 0.05) {
+                    if (currentMode.equals(mode.ManualClose)) {
+                        currentMode = mode.Auto;
+                    } else {
+                        currentMode = mode.ManualClose;
+                    }
+                }
+            }
+
+            // set the previousPressed conditionals. separate from the logic for clarity.
+            isLongPrevPressed = false;
+            isShortPrevPressed = false;
             if (gamepad1.left_bumper) {
+                isLongPrevPressed = true;
+            }
+            if (gamepad1.left_trigger > 0.05) {
+                isShortPrevPressed = true;
+            }
+
+            // set the LED actions based on the current robot LED mode
+            if (currentMode.equals(mode.ManualFar)) {
                 robot.wheels.rpmReset(Robot.Distance.Long.RPM);
                 robot.modeLed.setEasingMode(LEDLight.LightMode.Flat);
                 robot.modeLed.setFlatColor(LEDLight.ColorValues.Red.color + 0.03);
                 robot.modeLed.easingTick();
-            } else if (gamepad1.left_trigger > 0.05) {
+            } else if (currentMode.equals(mode.ManualClose)) {
                 robot.wheels.rpmReset(Robot.Distance.Short.RPM);
                 robot.modeLed.setEasingMode(LEDLight.LightMode.Flat);
                 robot.modeLed.setFlatColor(LEDLight.ColorValues.Green.color - 0.10);
                 robot.modeLed.easingTick();
-            } else {
+            } else if (currentMode.equals(mode.Auto)){
                 robot.autoTagSwap(Robot.tags.blueTeamGoal, Robot.tags.redTeamGoal);
             }
 
