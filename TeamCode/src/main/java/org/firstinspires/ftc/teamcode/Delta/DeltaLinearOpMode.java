4c05d71b3110c85fc34a98f3c06dee54c0b12bf2
Lisa <lm.merritt@yahoo.com>
2026-06-16 19:32:23 -0500

    workshopping delta implementations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Delta/DeltaLinearOpMode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Delta;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class DeltaLinearOpMode extends LinearOpMode {
+
+    /**
+     * override necessity, override in your mode and use as normal.
+     * @throws InterruptedException
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {}
+
+    @Override
+    public void waitForStart() {
+        super.waitForStart();
+    }
+
+
+}
