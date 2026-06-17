cd5cfea2fe6b2a6326ca64bd8bccb6152ed3b870
kahvi <kahvidsachi@gmail.com>
2026-06-16 19:08:22 -0500

    Simplifying code by making a new function that condenses the code that will be used to make the robot move while spinning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Unused/FieldTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Unused/FieldTeleOp.java
@@ -5,14 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Assemblies.DriveTrainMecanum;
 
-@TeleOp (name = "FieldOrientedTeleOp", group = "LinearOpMode")
+@TeleOp (name = "FieldOriented", group = "LinearOpMode")
 public class FieldTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
         DriveTrainMecanum driveTrain = new DriveTrainMecanum(hardwareMap, telemetry);
         waitForStart();
         while (opModeIsActive()) {
-            driveTrain.fieldOriented(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            driveTrain.fieldOriented(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
         }
     }
 }
