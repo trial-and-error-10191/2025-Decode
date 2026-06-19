0f9722c1ee60ea4b13b39a2b2e7dd1a31f5e988b
kahvi <kahvidsachi@gmail.com>
2026-06-18 20:59:30 -0500

    Editing the spin auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SpinAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SpinAuto.java
@@ -10,9 +10,14 @@ public class SpinAuto extends LinearOpMode {
     public void runOpMode() {
         Robot robot = new Robot(hardwareMap, telemetry);
         waitForStart();
-        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, 1, -0.3, 1, 3.5);
-        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -0.3, -1, 1, 7);
-        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -1, -0.3, 1, 7);
-        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -0.3, 1, 1, 1.5);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, 1, -0.3, 1, 3.3);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -0.3, -1, 1, 5.7);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -1, -0.1, 1, 5);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -0.3, 1, 1, 1.9);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, 1, -0.1, 1, 3.4);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -0.3, 1, 1, 1.8);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -1, 0.1, 1, 3);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, -0.3, 1, 1, 2.7);
+        robot.autoBase.SitAndSpin(robot.driveTrainMecanum, 1, 0.1, 1, 3.4);
     }
 }
